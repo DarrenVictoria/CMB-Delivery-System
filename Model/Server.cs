bef15293fb3402c8f982f699f4eb44cd0b9973f7
Pasan Anuththara <pasananuththara19@gmail.com>
2023-07-28 01:40:23 +0530

    DAO, User, and User Verification

---
+++ Model/Server.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMB_Delivery_Management.Model
+{
+    internal static class Server
+    { 
+        internal static String Name { get; set; } = "TECXICK-LAPTOP";
+    }
+}
